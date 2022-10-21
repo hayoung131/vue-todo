9b79add5933bb361e0d783da6718e1a5eef4845b
hayoung131 <hayoung131@gmail.com>
2022-10-21 23:56:15 +0900

    update : append TodoInput input and button

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>input</div>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">추가</button>
+  </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        newTodoItem : ""
+      }
+    },
+    methods : {
+      addTodo() {
+        console.log(this.newTodoItem)
+      }
+    }
   }
 </script>
 
