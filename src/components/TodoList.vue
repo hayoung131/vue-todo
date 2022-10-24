3a082c58823b1600de469f38fca38275c90b453c
hayoung131 <hayoung131@gmail.com>
2022-10-24 23:23:13 +0900

    update : append remove logic and list style

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,13 @@
 <template>
   <section>
     <ul>
-      <li v-for="todoItem in todoItems">{{ todoItem }}</li>
+      <li v-for="(todoItem, index) in todoItems" class="shadow" :key="todoItem">
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{todoItem}}
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+          <i class="far fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
     </ul>
   </section>
 </template>
@@ -19,9 +25,41 @@
           this.todoItems.push(localStorage.key(i));
         }
       }
+    },
+    methods : {
+      removeTodo : function (item, index) {
+        localStorage.removeItem(item);
+        this.todoItems.splice(index, 1);
+      }
     }
   }
 </script>
 
-<style>
+<style scoped>
+ ul {
+    list-style-type : none;
+    padding-left : 0px;
+    margin-top : 0;
+    text-align : left;
+  }
+  li {
+    display : flex;
+    min-height : 50px;
+    height : 50px;
+    line-height: 50px;
+    margin : 0.5rem 0;
+    padding : 0 0.9rem;
+    background : white;
+    border-radius : 5px;
+  }
+  .checkBtn {
+    line-height : 45px;
+    color : #62acde;
+    margin-right : 5px;
+  }
+
+  .removeBtn {
+    margin-left : auto;
+    color : #de4343;
+  }
 </style>
