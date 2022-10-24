d0aab1b8eb3611b2a770dfc750ca7becb4063356
hayoung131 <hayoung131@gmail.com>
2022-10-24 22:50:41 +0900

    update : append list logic

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="todoItem in todoItems">{{ todoItem }}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        todoItems : []
+      }
+    },
+    created()  {
+      if (localStorage.length > 0) {
+        for (var i=0 ; i < localStorage.length ; i++) {
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
   }
 </script>
 
