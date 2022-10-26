62144f18aa097d2ef1bd5cedf458b0788c8a07a9
hayoung131 <hayoung131@gmail.com>
2022-10-26 23:55:26 +0900

    update : fix so that it is reflected without refreshing when creating or deleting

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
   </div>
 </template>
 
@@ -13,6 +13,34 @@
   import TodoList from "./components/TodoList.vue"
   import TodoFooter from "./components/TodoFooter.vue"
   export default {
+    data () {
+      return {
+        todoItems : []
+      }
+    },
+    created()  {
+      if (localStorage.length > 0) {
+        for (var i=0 ; i < localStorage.length ; i++) {
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    },
+    methods : {
+      addTodo(todoItem) {
+        // 로컬 스토리지에 데이터를 추가하는 로직
+        console.log(todoItem);
+        localStorage.setItem(todoItem, todoItem);
+        this.todoItems.push(todoItem);
+      },
+      clearAll() {
+        localStorage.clear();
+        this.todoItems = [];
+      },
+      removeTodo (item, index) {
+        localStorage.removeItem(item);
+        this.todoItems.splice(index, 1);
+      }
+    },
     components : {
       "TodoHeader" : TodoHeader,
       "TodoInput" : TodoInput,
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -17,9 +17,13 @@
     methods : {
       addTodo() {
         if (this.newTodoItem !== "") {
-          console.log(this.newTodoItem);
-          localStorage.setItem(this.newTodoItem, this.newTodoItem);
+          this.$emit("addTodo", this.newTodoItem); // addTodo 이벤트를 발생시키면서 value 값을 TodoInput child teg에 전달한다.
           this.clearInput();
+          // 등록 및 삭제시 바로 반영되지 않음.
+          // console.log(this.newTodoItem);
+          // localStorage.setItem(this.newTodoItem, this.newTodoItem);
+          // this.clearInput();
+
         }
       },
       clearInput() {
