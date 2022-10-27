8209fa0750a84540f53e9d432f876d35f767e4a8
hayoung131 <hayoung131@gmail.com>
2022-10-27 23:50:31 +0900

    update : vue animated whenever item was appended and deleted

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,14 @@
 <template>
   <section>
-    <ul>
-      <li v-for="(todoItem, index) in propsdata" class="shadow" :key="todoItem">
-        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
-        {{todoItem}}
-        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
-          <i class="far fa-trash-alt" aria-hidden="true"></i>
-        </span>
-      </li>
-    </ul>
+      <transition-group name="list" tag="ul">
+        <li v-for="(todoItem, index) in propsdata" class="shadow" :key="todoItem">
+          <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+          {{todoItem}}
+          <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+            <i class="far fa-trash-alt" aria-hidden="true"></i>
+          </span>
+        </li>
+      </transition-group>
   </section>
 </template>
 
@@ -24,6 +24,14 @@
 </script>
 
 <style scoped>
+.list-enter-active, .list-leave-active {
+  transition : all 1s;
+}
+
+.list-enter, .list-leave-to {
+  opacity : 0;
+  transform : translateY(30px);
+}
  ul {
     list-style-type : none;
     padding-left : 0px;
